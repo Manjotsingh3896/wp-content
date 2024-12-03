60cb9618775ffba927ee56772f171d6c393e9fb3
Manjot Singh <manjotsingh3896@gmail.com>
2024-12-03 09:54:07 -0500

    query loop created

--- themes/beautiful-child/functions.php
+++ themes/beautiful-child/functions.php
@@ -11,4 +11,11 @@
 function child_enqueue_scripts() {
     wp_enqueue_style( 'child-style', get_stylesheet_uri());
 }
-add_action( 'wp_enqueue_scripts', 'child_enqueue_scripts');
+    add_action( 'wp_enqueue_scripts', 'child_enqueue_scripts');
+
+    function enqueue_logged_in_styles() {
+    if (is_user_logged_in()) {
+        wp_enqueue_style('logged-in-css', get_stylesheet_directory_uri() . '/beautifull.css', array(), '1.0.0');
+    }
+}
+add_action('wp_enqueue_scripts', 'enqueue_logged_in_styles');
